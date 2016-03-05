49d097c8c17b256682d97b9f24b82b0e3e5f1eda
martin36 <martin.funkquist@gmail.com>
2016-03-05 21:42:59 +0100

    Fixed firing of canon.

---
+++ js/model/canonball.js
@@ -0,0 +1,14 @@
+﻿var Canonball = function () {
+
+	var position = new Victor();
+	var velocity = new Victor();
+
+	//Input of type Victor
+	this.setPosition = function (pos) { position = pos; };
+	this.getPosition = function () { return position; };
+	//Input of type Victor
+	this.setVelocity = function (vel) { velocity = vel; };
+	this.getVelocity = function () { return velocity; };
+
+	return this;
+};
--- js/model/model.js
+++ js/model/model.js
@@ -2,10 +2,11 @@ var Model = function () {
 
 	var players = [];
 	var environment = new Environment();
+	var canonballs = [];
 	var randomizePos = false;
+	var canonballSpeed = 10;
 
 	this.addPlayer = function (name) {
-		//TODO: Fill list with the nr of players specified
 		var player = new Player();
 		player.setName(name);
 		//Randomize position?
@@ -32,11 +33,13 @@ var Model = function () {
 
 	// Vector algebra by using the Victor package
 	this.update = function (dt) {
-		// TODO: get wind direction and strength from environment.
+
 		var windDirection = environment.getWindDirection();
 		var windMagnitude = environment.getWindMagnitude();
-		var scalar = new Victor();			//Vector to represent distance scalar in vector multiplication
-		for(var i = 0; i < players.length; i++){
+		var windVelocity = environment.getWindVelocity();
+		var scalar = new Victor();			//Vector to represent distance scalar in vector multiplication(needed for Victor package)
+
+		for (var i = 0; i < players.length; i++) {
 			var cosOfAngle = windDirection.dot(players[i].getDirection());		//The cos-value of the angle between the wind direction and the direction of the boat
 			var speed = windMagnitude * cosOfAngle;		//If the wind is parallell to the boat then the speed becomes equal to the magnitue of the wind, if it is perpendicular then it becomes 0	
 			players[i].setSpeed(speed);		
@@ -48,13 +51,61 @@ var Model = function () {
 			scalar.y = distance;
 			var distanceVector = scalar.multiply(dir);		//The lenght and direction to move the player
 			players[i].setPosition(currPos.add(distanceVector));
-
 		}
-		// calculate new position of all players.
-		// Update things....
+
+		for (var i = 0; i < canonballs.length; i++) {
+			var position = canonballs[i].getPosition();
+			var velocity = canonballs[i].getVelocity();
+			var newVelocity = velocity.add(windVelocity);		//Calculate the new velocity depending on the wind velocity
+			var distanceVector = new Victor(dt, dt).multiply(newVelocity);
+			canonballs[i].setPosition(position.add(distanceVector));
+			document.write(canonballs[i].getPosition().toString());
+		}
+		checkForCollisions();
+
 		// Should Controller contain a gameloop which calls this??
 	}
+	//Turns the player in the specified direction
+	//Give the direction input as a string
+	this.turnPlayer = function(playerNr, direction){
+		if(playerNr > players.length || playerNr < 0) {		//Safety check
+			alert("Index out of bounds! Please try again");
+		}
+		if(direction === "right"){
+			players[playerNr].rotateRight();
+		}
+		else if (direction === "left") {
+			players[playerNr].rotateLeft();
+		}
+		else {
+			alert("That's not a valid direction! Try again.")
+		}
+	};
+	//Function for firing the cannon
+	this.fire = function (playerNr) {
 
+		var position = players[playerNr].getPosition();
+		var playerDirection = players[playerNr].getDirection();
+
+		//The canonballs should be fired in the perpendicular direction to the boat
+		var canonball1 = new Canonball();
+		var canonball2 = new Canonball();
+		canonball1.setPosition(position);
+		canonball2.setPosition(position);
+
+		//The vector [x, y] have the orthogonal vector [y, -x] for arbitrary values of x and y the reversed vector of [y, -x] is [-y, x]
+		//The multiplication with the canonball speed makes sure that the velocity for the canonball is correct
+		var velocity1 = new Victor(playerDirection.y * canonballSpeed, -playerDirection.x * canonballSpeed);
+		var velocity2 = new Victor(-playerDirection.y * canonballSpeed, playerDirection.x * canonballSpeed);
+		canonball1.setVelocity(velocity1);
+		canonball2.setVelocity(velocity2);
+		canonballs.push(canonball1);
+		canonballs.push(canonball2);
+
+	};
+	var checkForCollisions = function () {
+		//TODO: Check for intersection between canonballs and boats
+	}
 	return this;
 
 }
