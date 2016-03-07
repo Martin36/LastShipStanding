b35c72adbe03f159ca642dfbec27c348a1382124
martin <martin.funkquist@gmail.com>
2016-03-07 16:49:15 +0100

    Warning! Untested update. Try with caution.

--- js/model/player.js
+++ js/model/player.js
@@ -8,21 +8,12 @@ var Player = function () {
 		hp = 0,
 		deltaA = 0.0,
 		dead = false;
+		keyBindings = []
 
 	// Give variables standard values
 	hp = 100;
 	deltaA = 15;
 
-	this.setName = function (n) { name = n; };
-	this.getName = function () { return name; };
-	this.setDirection = function (direction) { dir = direction; };
-	this.getDirection = function () { return dir; };
-	this.setPosition = function (p) {
-		pos.x = p.x;
-		pos.y = p.y;
-	};
-	this.getPosition = function () { return pos; };
-	this.setAngle = function (a) { angle = a; }; // Will we use this or rotate?
 	this.rotateLeft = function () {
 		angle += deltaA;
 		if (angle > 360) {
@@ -35,17 +26,29 @@ var Player = function () {
 			angle += 360;
 		}
 	};
-	this.getAngle = function () { return angle; };
-	this.setSpeed = function (s) { speed = Math.abs(s); };
-	this.getSpeed = function () { return speed; };
 	this.takeDamage = function () {
 		hp -= 10;
 		if (hp <= 0) {
-			this.dead = true;
+			dead = true;
 		}
 	};
-	this.getHp = function () { return hp; };
 	this.isDead = function () { return dead; };
+
+	this.setName = function (n) { name = n; };
+	this.getName = function () { return name; };
+	this.setDirection = function (direction) { dir = direction; };
+	this.getDirection = function () { return dir; };
+	this.setPosition = function (p) {
+		pos.x = p.x;
+		pos.y = p.y;
+	};
+	this.getPosition = function () { return pos; };
+	this.setAngle = function (a) { angle = a; }; // Will we use this or rotate?
+	this.getAngle = function () { return angle; };
+	this.setSpeed = function (s) { speed = Math.abs(s); };
+	this.getSpeed = function () { return speed; };
+	this.getHp = function () { return hp; };
+	this.setKeyBindings(keys) {}
 	return this;
 }
 
