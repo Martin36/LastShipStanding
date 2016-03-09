ab983148413a8e46680849f1909b372072342c73
Anton E <Eldhen@hotmail.com>
2016-03-09 16:19:36 +0100

    Comments removed, again

--- js/controller/playController.js
+++ js/controller/playController.js
@@ -1,13 +1,6 @@
 var playController = function(view,model) {
 	
-	//Assign View keys up here.
 	Players = model.getPlayers();
-	//alert(Players[0].getName()+ " " + Players[1].getName()+ " " + Players[2].getName()+ " " + Players[3].getName()+ " ");
-	//Players[0].setKeyBindings([65, 87, 68]);
-	//Players[1].setKeyBindings([70, 84, 72]);
-	//Players[2].setKeyBindings([74, 73, 76]);
-	//Players[3].setKeyBindings([37, 38, 39]);
-	//alert( Players[0].getKeyBindings()[0]);
 	
 	var interValID;
 
@@ -35,12 +28,8 @@ var playController = function(view,model) {
 		
 		for(i in Players){
 			var keys = Players[i].getKeyBindings();
-<<<<<<< HEAD
-=======
 			console.log(keys);
 			console.log(Players[i].getName());
-			//console.log(keys[0] + " " + keys[1] + " " + keys[2]);
->>>>>>> 97c43e69d5665752a6712a8d496f4d0231973f9b
 			if (map[keys[0]])	{ Players[i].rotateLeft();   /*console.log(Players[i].getName()+"'s Going left!");*/ }
 			if (map[keys[1]])	{ Players[i].fire();		 /*console.log(Players[i].getName()+"'s shots fired!!");*/}
 			if (map[keys[2]])	{ Players[i].rotateRight();  /*console.log(Players[i].getName()+"'s Going right!");*/ }
@@ -48,7 +37,6 @@ var playController = function(view,model) {
 	}
 	
 	function timer(){
-		//model.update(17);
 		model.update(1);
 		view.update();
 	}
