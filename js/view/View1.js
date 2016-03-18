87bc565627f36fd3e4ed848f5602304561ca600e
martin <martin.funkquist@gmail.com>
2016-03-18 16:18:54 +0100

    Deleted old files.

--- js/view/View1.js
+++
@@ -1,4 +0,0 @@
-var View1 = function (model) {
-	this.playBtn = $("#view1_playBtn");
-	this.settingsBtn = $("#view1_settingsBtn");
-}
--- js/view/View2.js
+++
@@ -1,4 +0,0 @@
-//View Object constructor
-var View2 = function (model) {
-	this.testBtn = $("#view2_2players");
-}
