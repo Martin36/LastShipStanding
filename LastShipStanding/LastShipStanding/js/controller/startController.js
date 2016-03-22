06c83c4a0eb35f44c780f4d4e5dfa5c7e41579cb
Julian Hedén <julianheden@gmail.com>
2016-03-22 22:54:56 +0100

    fadeIn for bgAudio in view1

--- LastShipStanding/LastShipStanding/js/controller/startController.js
+++ LastShipStanding/LastShipStanding/js/controller/startController.js
@@ -2,6 +2,7 @@
 
     // Loop the bg music
     model.getBgAudio().loop = true;
+    model.getBgAudio().volume = 0.05;
     model.getBgAudio().play();
     var i = true;
 
@@ -23,8 +24,20 @@
         }
     };
 
-	/*view.settingsBtn[0].onclick = function() {
-		$("[id=view1]").hide(); //main menu
-		$("[id=view3]").show();	//settings menu
-	};*/
+    var vol = 0.05;
+    var fadeIn = setInterval(
+    function() {
+        // Reduce volume by 0.05 as long as it is above 0
+        // This works as long as you start with a multiple of 0.05!
+        if (vol < 1) {
+            model.getBgAudio().volume = vol;
+            vol += 0.05;
+        }
+        else {
+            // Stop the setInterval when 0 is reached
+            clearInterval(fadeIn);
+        }
+    }, 400);
+
+
 }
